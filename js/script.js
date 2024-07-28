0405146a1c1ed0c7f5af8eb28d8c7f9c4d7c33b7
SterrenHeer <sterrenheer@gmail.com>
2024-07-28 21:48:51 +0300

    add counters section and fix some elements

--- js/script.js
+++ js/script.js
@@ -4,3 +4,23 @@ $('.burger_button, .burger_close, .burger a, .burger button').click(() => {
 $(window).on( "resize", () => {
     $('.burger').removeClass('flex');
 });
+
+$(document).on('scroll DOMContentLoaded', function() {
+    if($(this).scrollTop() + window.innerHeight >= $('.counters_items').offset().top) {
+        $('.counters_item span').each(function() {
+            let i = 1,
+                num = $(this).data('num'),
+                that = $(this),
+                int = setInterval(function() {
+                    if (i <= num && i > that.html()) {
+                        that.html(i);
+                    } else if (i > num) {
+                        that.html(num);
+                    } else {
+                        clearInterval(int);
+                    }
+                    num > 2000 ? i = i + 3 : i++;
+                }, 5000 / num);
+        });
+    }
+});
